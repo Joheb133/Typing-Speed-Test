6193613414a608494ad93fecbd8b3759bf0d42b7
zoheb598 <joheb133@gmail.com>
2021-10-15 00:15:39 +0100

    Simple typing test implemented
    A list of items is shown, if the user spells a word correctly in the list, it is removed from the list and page.

--- app.js
+++ app.js
@@ -2,8 +2,31 @@
 // input is fed to js
 // input has to match output
 
-const inputEl = document.getElementById("input-el");
+let words = [
+    "hello", "world", "think", "iron",
+    "love", "music", "genius", "prevent",
+]
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 inputEl.addEventListener("keyup", function() {
-    console.log(inputEl.value)
+    for(i = 0; i < words.length; i++){
+        if(inputEl.value === words[i]){
+            words.splice(i, 1)
+            render()
+            inputEl.value = "";
+        }
+    }
 })
+
+function render(){
+    let listItems = "";
+    for(i = 0; i < words.length; i++){
+        listItems += `
+        <li> ${words[i]} </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+render()
