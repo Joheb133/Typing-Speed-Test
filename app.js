6d329401e64093440fffc7c98467312623bf5bb2
zoheb598 <joheb133@gmail.com>
2021-10-14 21:19:53 +0100

    input element logged into console

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // Need some kind of input
 // input is fed to js
 // input has to match output
+
+const inputEl = document.getElementById("input-el");
+
+inputEl.addEventListener("keyup", function() {
+    console.log(inputEl.value)
+})
