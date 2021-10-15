c7a97925812774e75fbd35c7c1e26208cba033ab
zoheb598 <joheb133@gmail.com>
2021-10-15 22:14:05 +0100

    Removed min max char + created a large list

--- app.js
+++ app.js
@@ -1,68 +1,56 @@
-// Need some kind of input
-// input is fed to js
-// input has to match output
 
+let wordsBuffer = [
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+    "hello", "world", "think", "iron",
+]
 let words = [
     "hello", "world", "think", "iron",
-    "love", "music", "genius", "prevent",
-    "hello", "world", "think", "iron",
-    "love", "music", "genius", "prevent",
-    "hello", "world", "think", "iron",
-    "love", "music", "genius", "prevent",
-    "hello", "world", "think", "iron",
-    "love", "music", "genius", "prevent",
-    "hello", "world", "think", "iron",
-    "love", "music", "genius", "prevent",
-    "hello", "world", "think", "iron",
-    "love", "music", "genius", "prevent",
 ]
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-const charMax = arrayStringMax(words)
-const charMin = arrayStringMin(words)
-
-// Array character max
-function arrayStringMax(arr) { 
-    let max = 0;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].length > max) {
-            max = arr[i].length;
-        }
-    }
-    return max
-}
-
-// Array character min
-function arrayStringMin(arr) { 
-    let min = Infinity;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].length < min) {
-            min = arr[i].length
-        }
-    }
-    return min
-}
-
-// If key pressed run through loop
 inputEl.addEventListener("keyup", function () {
-        for (i = 0; i < words.length; i++) {
-            if (inputEl.value === words[i]) {
-                words.splice(i, 1)
-                render()
-                inputEl.value = ""
-            }
-        }
+    if (inputEl.value === words[0]) {
+        words.splice(0, 1)
+        render()
+        inputEl.value = ""
+    }
 })
 
 function render() {
-    let listItems = "";
+    let listItems = ""
     for (i = 0; i < words.length; i++) {
         listItems += `
         <li> ${words[i]} </li>
         `
     }
     ulEl.innerHTML = listItems
+    inputEl.placeholder = words[0]
 }
 
 render()
