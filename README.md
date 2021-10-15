fc7a4fb03f7c99dff0dec9ae72acc24a562078da
zoheb598 <joheb133@gmail.com>
2021-10-15 23:23:00 +0100

    Removed render function
    Replaced render function with firstElementChild.remove() because there's no need to loop through the whole array in order to update the document.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Typing Speed Test
  A website where user's can measure their typing speed in wpm
+
+# Problems I've encountered
+ I want to implement user input progression. My idea to use manipulate placeholder doesn't seem to be going anywhere.
+ After doing some research it look's like similar websites don't actually show the users input in an input bar.
+ 
--- app.js
+++ app.js
@@ -27,31 +27,31 @@ let wordsBuffer = [
     "hello", "world", "think", "iron",
     "hello", "world", "think", "iron",
     "hello", "world", "think", "iron",
-]
+];
 let words = [
     "hello", "world", "think", "iron",
-]
-const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ul-el")
+];
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+words = wordsBuffer;
+
 
 inputEl.addEventListener("keyup", function () {
     if (inputEl.value === words[0]) {
-        words.splice(0, 1)
-        render()
-        inputEl.value = ""
-    }
-})
+        words.splice(0, 1);
+        ulEl.firstElementChild.remove();
+        inputEl.value = "";
+    };
+});
 
-function render() {
-    let listItems = ""
+(function() {
+    let listItems = "";
     for (i = 0; i < words.length; i++) {
         listItems += `
         <li> ${words[i]} </li>
         `
-    }
-    ulEl.innerHTML = listItems
-    inputEl.placeholder = words[0]
-}
-
-render()
+    };
+    ulEl.innerHTML = listItems;
+}());
 
