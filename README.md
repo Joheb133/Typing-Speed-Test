4e462803731bd02b24b4c22a5cf9068ab45dcdd7
zoheb598 <joheb133@gmail.com>
2021-10-18 16:59:08 +0100

    Added the conditions to show the current character if it equals the character in string at corresponding position
    I created two variables. The first gives the input position. The second get's the character at input position.
    
    Using those variables I created an if statement where, if the current character in input = the character in the string from word[] then do something.
    
    For now I'm just logging the character in the console.

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 # Problems I've encountered
  I want to implement user input progression. My idea to use manipulate placeholder doesn't seem to be going anywhere.
  After doing some research it look's like similar websites don't actually show the users input in an input bar.
+
+ I need to chose whether to overlay an element with the same style ontop of the output or actually change the opacity of the output in js
  
--- app.js
+++ app.js
@@ -33,25 +33,34 @@ let words = [
 ];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const ulOverlayEl = document.getElementById("ul-overlay-el");
 
-words = wordsBuffer;
-
-
-inputEl.addEventListener("keyup", function () {
-    if (inputEl.value === words[0]) {
-        words.splice(0, 1);
-        ulEl.firstElementChild.remove();
-        inputEl.value = "";
-    };
-});
+//words = wordsBuffer;
 
 (function() {
     let listItems = "";
     for (i = 0; i < words.length; i++) {
-        listItems += `
-        <li> ${words[i]} </li>
-        `
+        listItems += `<li>${words[i]} </li>`
     };
     ulEl.innerHTML = listItems;
 }());
+
+
+inputEl.addEventListener("keyup", function () {
+    // *if input letter = letter from array in corresponding location
+    const inputPosition = (inputEl.value.length) - 1; // stores position in index
+    const inputCurrentValue = inputEl.value.charAt(inputPosition); // this stores the last input character
+    if(inputCurrentValue === words[0].charAt(inputPosition) && inputCurrentValue != ""){ // if the letters are equal at the same position
+        console.log(words[0].charAt(inputPosition))
+    }
+
+
+    if (inputEl.value === words[0]) {
+        words.splice(0, 1);
+        //ulEl.firstElementChild.remove();
+        inputEl.value = "";
+    };
+});
+
+
 
