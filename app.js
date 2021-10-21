b0e3520f2e81535632cdee43e4531f4acde6396e
zoheb598 <joheb133@gmail.com>
2021-10-21 22:47:01 +0100

    Changed code to add letters to document instead of words
    I removed the ul since I'm not going to remove words from the document.
    
    To show progression the letter will change in opacity or some other style to signify the user pressed the right key.
    
    To add letters one by one, I used a for loop that goes through every character of an item and adds it to the document.
    
    I left the progressionRender function at the bottom as this is what I'll be working on next.

--- app.js
+++ app.js
@@ -31,36 +31,36 @@ let wordsBuffer = [
 let words = [
     "hello", "world", "think", "iron",
 ];
+let letters = "";
 const inputEl = document.getElementById("input-el");
-const ulEl = document.getElementById("ul-el");
-const ulOverlayEl = document.getElementById("ul-overlay-el");
+const wordsEl = document.getElementById("words-el")
 
 //words = wordsBuffer;
 
-(function() {
-    let listItems = "";
-    for (i = 0; i < words.length; i++) {
-        listItems += `<li>${words[i]} </li>`
+//function adds letters from words array to document
+function documentLetters(item){
+    for(i = 0; i < item.length; i++){
+        letters += `<span>${item.charAt(i)}</span>`
     };
-    ulEl.innerHTML = listItems;
-}());
+    wordsEl.innerHTML = letters;
+};
+words.forEach(documentLetters);
 
-
+// key is pressed in input
 inputEl.addEventListener("keyup", function () {
-    // *if input letter = letter from array in corresponding location
-    const inputPosition = (inputEl.value.length) - 1; // stores position in index
-    const inputCurrentValue = inputEl.value.charAt(inputPosition); // this stores the last input character
-    if(inputCurrentValue === words[0].charAt(inputPosition) && inputCurrentValue != ""){ // if the letters are equal at the same position
-        console.log(words[0].charAt(inputPosition))
-    }
-
 
     if (inputEl.value === words[0]) {
         words.splice(0, 1);
-        //ulEl.firstElementChild.remove();
         inputEl.value = "";
     };
 });
+
+// Render to show user progression. Probably going to change each active spans opacity
+function progressionRender(){
+    const inputPosition = (inputEl.value.length) - 1; // stores position in index
+    // const inputCurrentValue = inputEl.value.charAt(inputPosition); // this stores the last input character
+
+}
 
 
 
