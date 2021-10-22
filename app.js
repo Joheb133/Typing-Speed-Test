82b9d61abadec186037345c3b8907b68d2dc8b07
zoheb598 <joheb133@gmail.com>
2021-10-22 16:17:45 +0100

    A span with space now separates words

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ let wordsBuffer = [
 let words = [
     "hello", "world", "think", "iron",
 ];
-let letters = "";
 const inputEl = document.getElementById("input-el");
 const wordsEl = document.getElementById("words-el")
 
@@ -39,20 +38,18 @@ const wordsEl = document.getElementById("words-el")
 
 //function adds letters from words array to document
 function documentLetters(item){
+    let letters = "";
     for(i = 0; i < item.length; i++){
         letters += `<span>${item.charAt(i)}</span>`
     };
-    wordsEl.innerHTML = letters;
+    letters += `<span> </span>`;
+    wordsEl.innerHTML += letters;
 };
 words.forEach(documentLetters);
 
 // key is pressed in input
 inputEl.addEventListener("keyup", function () {
-
-    if (inputEl.value === words[0]) {
-        words.splice(0, 1);
-        inputEl.value = "";
-    };
+    
 });
 
 // Render to show user progression. Probably going to change each active spans opacity
