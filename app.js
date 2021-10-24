5330df68a2f2a66f0aa517a93106e8d899df472c
zoheb598 <joheb133@gmail.com>
2021-10-24 20:29:27 +0100

    Fixed issue where pressing any key moves the test forward + added the ability to use backspace
    I have two event listeners attached to the document in order to keep the letter, punctuation and number keys separate from values like backspace. I don't know if this is good practice though.
    
    In any case, separating to two event listeners allowed for the use of less conditional statements which seems to have increased responsiveness.

--- app.js
+++ app.js
@@ -32,14 +32,15 @@ let words = [
     "hello", "world", "think", "iron",
 ];
 let keyPosition = 0;
-const wordsEl = document.getElementById("words-el")
+const wordsEl = document.getElementById("words-el");
+let wordsCurrentValue = wordsEl.children[keyPosition];
 
 //words = wordsBuffer;
 
 //function adds letters from words array to document
-function documentLetters(item){
+function documentLetters(item) {
     let letters = "";
-    for(i = 0; i < item.length; i++){
+    for (i = 0; i < item.length; i++) {
         letters += `<span>${item.charAt(i)}</span>`
     };
     letters += `<span> </span>`;
@@ -48,25 +49,29 @@ function documentLetters(item){
 words.forEach(documentLetters);
 
 // key is pressed in input
-document.addEventListener("keyup", function (event) {
-    keyValue = event.key
+document.addEventListener("keypress", function (event) {
+    keyPressValue = event.key
     progressionRender();
+});
+// keypressed for backspace
+document.addEventListener("keyup", function (event) {
+    keyUpValue = event.key
+    if (keyUpValue === "Backspace" && keyPosition > 0) {
+        keyPosition--;
+        wordsCurrentValue = wordsEl.children[keyPosition];
+        wordsCurrentValue.id = "";
+    }
 });
 
 // Render to show user progression. Probably going to change each active spans opacity
-function progressionRender(){
-    console.log(keyValue)
-    if(keyValue === "Backspace" && keyPosition > 0){
-        keyPosition--;
-    } else {
-        keyPosition++;
-    }
-    let wordsCurrentValue = wordsEl.children[keyPosition-1];
-    
-    // If the letter at inputs position is at is equal to the span at the same position as input position then execute code 
-    if( keyValue === wordsCurrentValue.textContent){
+function progressionRender() {
+    wordsCurrentValue = wordsEl.children[keyPosition];
+    // If the letter at inputs position is at is equal to the span at the same position as input position then execute code
+    if (keyPressValue === wordsCurrentValue.textContent) {
         wordsCurrentValue.id = "right-span";
-    } else{
-        wordsCurrentValue.id = "wrong-span"
+    } else {
+        wordsCurrentValue.id = "wrong-span";
     }
+    keyPosition++;
+
 };
