be69fbcbd06b724063ed9eb0c197f7ee5fa4ae3d
zoheb598 <joheb133@gmail.com>
2021-11-03 23:38:00 +0000

    Added timer
    It starts when the user pressed a key. When it ends the user can't type

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const words = [
-    'Machines', 'like', 'Robert', 'are', 'mainstays', 'of', 'science', 
-    'fiction-the', 'idea', 'of', 'a', 'robot', 'that', 'somehow', 'replicates', 
-    'consciousness', 'through', 'its', 'hardware', 'or', 'software', 'has', 
-    'been', 'around', 'so', 'long', 'it', 'feels', 'familiar.']
-;
+    'Machines', 'like', 'Robert', 'are', 'mainstays', 'of', 'science',
+    'fiction-the', 'idea', 'of', 'a', 'robot', 'that', 'somehow', 'replicates',
+    'consciousness', 'through', 'its', 'hardware', 'or', 'software', 'has',
+    'been', 'around', 'so', 'long', 'it', 'feels', 'familiar.'
+];
 const wordsEl = document.getElementById("words-el");
-const paragraphLength = wordsEl.childElementCount;
+
 let keyPosition = 0;
 let wordsCurrentValue = wordsEl.children[keyPosition];
 
@@ -19,29 +19,33 @@ function documentLetters(item) {
     wordsEl.innerHTML += letters;
 };
 words.forEach(documentLetters);
-
+const paragraphLength = wordsEl.childElementCount;
 
 // Two event listeners attached to document because: Keypress only listens for letters, numbers and punctuations. This stops the need to check for specific keys.
 // I still need the backspace and that's what the second event listener is for.
-document.addEventListener("keypress", function (event) {
-    if(keyPosition < paragraphLength-1){ // makes sure not to run function when there's no more letters
-        keyPressValue = event.key
+document.addEventListener("keypress", userKeyPress);
+
+function userKeyPress(e) {
+    if (keyPosition < paragraphLength - 1) { // makes sure not to run function when there's no more letters
+        keyPressValue = e.key;
         progressionForward();
     };
-});
+};
 
-document.addEventListener("keydown", function (event) {
-    keyUpValue = event.key
+document.addEventListener("keydown", userKeyDown);
+
+function userKeyDown(e) {
+    keyUpValue = e.key
     if (keyUpValue === "Backspace" && keyPosition > 0) {
         progressionBackward();
-    }
-});
+    };
+};
 
 // Code for when the user presses a key
 function progressionForward() {
     wordsCurrentValue = wordsEl.children[keyPosition]; // update var since key position changes
     keyPosition++; // increase keyPosition since a key was pressed
-    
+
     // code below is less for function but more about giving the user feedback
     // indicate current user placement
     wordsEl.children[keyPosition].style.borderBottom = "solid blue";
@@ -56,12 +60,38 @@ function progressionForward() {
 };
 
 // Code for when the user presses backspace
-function progressionBackward(){
+function progressionBackward() {
     keyPosition--;
     wordsCurrentValue = wordsEl.children[keyPosition]; //update var
     // indicate current user placement
     wordsCurrentValue.style.borderBottom = "solid blue";
-    wordsEl.children[keyPosition+1].style.borderBottom = "none";
+    wordsEl.children[keyPosition + 1].style.borderBottom = "none";
 
     wordsCurrentValue.id = ""; // remove right/wrong styling
+};
+
+// Timer is being kept here for now since I see no reason for it to have its own file
+
+const timerEl = document.getElementById("timer-el");
+let time = 180;
+
+
+
+// Quick function that starts timer when a key is pressed then kills itself
+
+document.addEventListener("keydown", checkFirstInput);
+
+function checkFirstInput() {
+    // countdown timer
+    let timeInterval = setInterval(function () {
+        time--;
+        timerEl.textContent = time;
+        if (time <= 0) {
+            clearInterval(timeInterval);
+            // Kill event listeners to end game
+            document.removeEventListener("keypress", userKeyPress);
+            document.removeEventListener("keydown", userKeyDown);
+        };
+    }, 1000);
+    document.removeEventListener("keydown", checkFirstInput);
 };
